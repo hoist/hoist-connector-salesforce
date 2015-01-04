7b879684f18e229bc4cacab732fb30579fbe94d1
Eilis Byrne <eilis@hoistapps.com>
2015-01-05 11:38:53 +1300

    initial commit

---
+++ getting-started.js
@@ -0,0 +1,14 @@
+/* Just copy and paste this snippet into your code */
+
+module.exports = function (event, done) {
+
+  var salesforce = Hoist.connector('<key>');
+  salesforce.authorize().then(function () {
+    return get('SELECT Id, Name FROM Account');
+  }).then(function (result) {
+    for (var index = 0; index < result.length; index++) {
+      Hoist.event.raise('account:found', result.records[index]);
+    }
+  });
+
+};
