5378e0eb21808cffd9430223779d08a00c5eac82
Jamie Wilson <jamie@hoist.io>
2016-04-04 15:43:21 +1200

    view

--- src/views/edit.js
+++ src/views/edit.js
@@ -12,29 +12,43 @@ class EditForm extends C.View {
       this.state.mode = 'connect';
     }
   }
-  connect() {
-    this.props.onConnect();
-  }
   render() {
     return (
       <C.Page default="setup" {...this.props}>
         <C.Panel name="Setup" slug="setup">
-          <UI.FormElements.Button text={this.props.connectorInstance ? 'Reauthorize' : 'Connect'} type="large" onClick={()=>{
-              return this.connect();
-            }} />
+          <C.Column type="notes">
+            <h1>Adding a Salesforce Connector</h1>
+            <ol>
+              <li>Log in to <a href="https://login.salesforce.com/">https://login.salesforce.com/</a></li>
+              <li>Click <strong>'&lt;name&gt;'</strong>.</li>
+              <li>Click <strong>'My Settings'</strong>.</li>
+              <li>Click <strong>'Personal'</strong>.</li>
+              <li>Click <strong>'Reset My Security Token'</strong>.</li>
+              <li>Click <strong>'Reset Security Token'</strong>.</li>
+              <li>Go to your email and get the Security Token.</li>
+              <li>Enter your <strong>'Username'</strong> and <strong>'Password'</strong> into the boxes on this page.</li>
+              <li>Add the Security Token to the end of your password.</li>
+              <li>Click <strong>'Save & Verify'</strong>.</li>
+            </ol>
+          </C.Column>
+          <C.Column>
+            <form onChange={(evt) => {
+              this.props.updateField(evt);
+            }} onSubmit={(evt) => {
+              this.props.updateSettings(evt);
+            }}>
+              <UI.FormElements.Input inactive={!!(this.props.connectorInstance)} placeholder="Key" name="key" label="Key" type="text" value={this.props._key}/>
+              <UI.FormElements.Input placeholder="Username" name="username" label="Username" type="text" value={this.props.settings.username}/>
+              <UI.FormElements.Input placeholder="Password" name="password" label="Password" type="text" value={this.props.settings.password}/>
+              <UI.FormElements.Button
+                loading={this.props.saving}
+                text={this.props.connectorInstance ? 'Save' : 'Create'}
+                type="large"
+                submit={true}
+                onClick={this.props.updateSettings} />
+            </form>
+          </C.Column>
         </C.Panel>
-        {this.props.connectorInstance ? <C.Panel name="Events" slug="events">
-        <C.PageHeader
-          title="Check the boxes of the events you want to subscribe to."
-          subTitle="Checking a box will automatically subscribe you to that event." />
-          <C.CheckboxGrid
-            items={this.getAvailableEvents()}
-            checked={this.getSubscribedEvents()}
-            onChange={this.props.onSubscribe} />
-        </C.Panel> : <C.Panel name="Events" slug="events">
-          <C.EventsGrid.Header
-            title="Events are available once you've connected." />
-        </C.Panel>}
       </C.Page>
     );
   }
